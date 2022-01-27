6856697374f6b1f15f13ac637d8497078f86fa0a
Thomas Page <pagethom@ecs.vuw.ac.nz>
2022-01-27 21:00:23 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def guess_state(guess, solution):
+    pass
+
+
+print(guess_state("mecca", "cocks"))  # 0, 0, 2, 1, 0
+print(guess_state("foods", "cocks"))  # 0, 2, 0, 0, 2
+print(guess_state("kicks", "cocks"))  # 0, 0, 2, 2, 2
+print(guess_state("score", "cocks"))  # 1, 1, 1, 0, 0
